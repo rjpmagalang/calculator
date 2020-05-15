23287528a5b614e2398e44693fd11ec5577e03c3
Robert Magalang <rjpmagalang@users.noreply.github.com>
2020-05-15 22:44:35 +0800

    added button click

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-const four = document.querySelector(".four");
-const five = document.querySelector(".five");
-const six = document.querySelector(".six");
-const seven = document.querySelector(".seven");
-const eight = document.querySelector(".eight");
-const nine = document.querySelector(".nine");
-const zero = document.querySelector(".zero");
-
+const num = document.querySelectorAll(".num");
 const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
-const multiply = document.querySelector(".multiply");
-const divide = document.querySelector(".divide");
+const mul = document.querySelector(".mul");
+const div = document.querySelector(".div");
 const equals = document.querySelector(".equals");
-
 const clear = document.querySelector(".clear");
 const del = document.querySelector(".del");
 const plusMinus = document.querySelector(".plusMinus");
-
 const inputText = document.querySelector(".input-text");
 
-one.addEventListener("click", (e) => inputText.value = "1")
-clear.addEventListener("click", (e) => inputText.value = "0")
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+function operate(num1, num2, operator) {
+
+    num.forEach(div => {
+        div.addEventListener("click", (event) => {
+            inputText.value = event.target.textContent; 
+        })
+    });
+
+    clear.addEventListener("click", () => inputText.value = "0");
+}
+
+operate();
