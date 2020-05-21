9d858b938607664d3aaa64b63b26c42503fc1faa
Robert Magalang <rjpmagalang@users.noreply.github.com>
2020-05-21 15:43:36 +0800

    test using eval

--- script.js
+++ script.js
@@ -1,60 +1,61 @@
 const nums = document.querySelectorAll(".num");
 const operators = document.querySelectorAll(".operator");
+const point = document.querySelector(".point");
 const equals = document.querySelector(".equals");
 const clear = document.querySelector(".clear");
 const del = document.querySelector(".del");
 const plusMinus = document.querySelector(".plusMinus");
 const inputText = document.querySelector(".input-text");
 let operatorCount = 1;
+let pointCount = 1;
 let firstNum = "";
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+// const add = (a, b) => a + b;
+// const subtract = (a, b) => a - b;
+// const multiply = (a, b) => a * b;
+// const divide = (a, b) => a / b;
 
-function operate(a, b, operator) {
-    let prevNum = parseFloat(a);
-    let currNum = parseFloat(b);
-    let result = "";
+// function operate(a, b, operator) {
+//     let prevNum = parseFloat(a);
+//     let currNum = parseFloat(b);
+//     let result = "";
 
-    switch (operator) {
-        case "+":
-            result = add(prevNum, currNum);
-            break;
-        case "-":
-            result = subtract(prevNum, currNum);
-            break;
-        case "x":
-            result = multiply(prevNum, currNum);
-            break;
-        case "÷":
-            result = divide(prevNum, currNum);
-            break;
-        default:
-            break;
-    }
-    return result;
-}
+//     switch (operator) {
+//         case "+":
+//             result = add(prevNum, currNum);
+//             break;
+//         case "-":
+//             result = subtract(prevNum, currNum);
+//             break;
+//         case "x":
+//             result = multiply(prevNum, currNum);
+//             break;
+//         case "÷":
+//             result = divide(prevNum, currNum);
+//             break;
+//         default:
+//             break;
+//     }
+//     return result;
+// }
 
-function addNum(num) {
-    firstNum = num;
-    console.log(firstNum);
-    
-}
+// function addNum(num) {
+//     firstNum = num;
+// }
 
-function addOperation(operation) {
-    operator = operation;
-}
+// function addOperation(operation) {
+//     operator = operation;
+// }
 
-addNum();
+// addNum();
 
 function calculate() {
          nums.forEach(div => {
             div.addEventListener("click", (event) => {
                 inputText.value += event.target.textContent;
-                addNum(inputText.value);
-                operatorCount = 0;     
+                // addNum(inputText.value);
+                operatorCount = 0;
+                pointCount = 0;    
             })    
         });
 
@@ -63,6 +64,7 @@ function calculate() {
                 if(operatorCount === 0){
                     inputText.value += event.target.textContent;
                     ++operatorCount;
+                    pointCount = 0;
                 }
              })
          })
@@ -75,10 +77,18 @@ equals.addEventListener("click", () => {
 })
 
 del.addEventListener("click", () => {
-
+    inputText.value = inputText.value.slice(0, -1);
 })
 
 clear.addEventListener("click", () => {
     inputText.value = "";
     operatorCount = 1;
 });
+
+point.addEventListener("click", () => {
+    if(pointCount === 0 || inputText.value === ""){
+        inputText.value += ".";
+        ++pointCount;
+    }
+    
+})
